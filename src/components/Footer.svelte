e8bd86c8f426a31c99e9810e2ee620dc7bb38c0c
Lawrence N. Masullo <lmasullo@gmail.com>
2020-11-22 14:10:22 -0600

    Initial

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<style>
+</style>
+
+<footer>
+  <h3 class="fake-logo">Fake Logo</h3>
+  <nav>
+    <a href="##">Home</a>
+    <a href="##">About</a>
+    <a href="##">Contact</a>
+  </nav>
+  <p>&copy 2020 LPA</p>
+</footer>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<style>
+</style>
+
+<header>
+  <!-- <h1 class="fake-logo">Lampasas Pilots Association</h1> -->
+  <div>
+    <img src="/images/LPA_64.png" alt="LPA Logo" />
+    Lampasas Pilots Association
+  </div>
+
+  <nav>
+    <a href="##">Home</a>
+    <a href="##">About</a>
+    <a href="##">Meetings</a>
+    <a href="##">Contact</a>
+  </nav>
+</header>
