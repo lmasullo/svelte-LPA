64cb49dc41f0db5f79f640c7a3b41e279d701f51
Lawrence N. Masullo <lmasullo@gmail.com>
2021-08-18 10:16:50 -0500

    updated date

--- src/App.svelte
+++ src/App.svelte
@@ -43,7 +43,7 @@
   <div id="meeting" class="card">
     <h3>LPA Meeting</h3>
     <p>
-      Our next meeting will be Saturday, June 12, 2021, 0800.
+      Our next meeting will be Saturday, Sept 11, 2021, 0800.
       <br />
       The meeting will be held in a private room at Putter's and Gutters on 281 South
       in Lampasas, TX.
