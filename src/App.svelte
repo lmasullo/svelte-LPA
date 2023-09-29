cc1d5352c9459effd5190c096ec068bcfdad00f7
Lawrence Masullo <lmasullo@gmail.com>
2023-09-28 21:36:02 -0500

    Jan meeting

--- src/App.svelte
+++ src/App.svelte
@@ -43,7 +43,7 @@
   <div id="meeting" class="card">
     <h3>LPA Meeting</h3>
     <p>
-      Our next meeting will be Saturday, September 9, 2023, 0800.
+      Our next meeting will be Saturday, January 13, 2024, 0800.
       <br />
       The meeting will be held in a private room at Putter's and Gutters on 281 South
       in Lampasas, TX.
